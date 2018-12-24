dfcb347575cf81f19288033cd1eaf8b1d573cbd4
George Hall <george.hall@servian.com>
2018-12-24 15:15:27 +0000

    Initial commit

---
+++ src/main/java/com/george/testjar/TestJarApplication.java
@@ -0,0 +1,14 @@
+package com.george.testjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJarApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/george/testjar/TestJarApplicationTests.java
@@ -0,0 +1,17 @@
+package com.george.testjar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestJarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
